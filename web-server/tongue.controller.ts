45cd108814f47d3de0ba7c948029ce99fc12a98a
Terry Chan <chanhuiyiu@gmail.comgit config --global user.email chanhuiyiu@gmail.comgit config --global user.email chanhuiyiu@gmail.com>
2023-05-04 20:20:16 +0800

    add try catch in formaParse middleWare

--- web-server/tongue.controller.ts
+++ web-server/tongue.controller.ts
@@ -5,35 +5,39 @@ import { Request, Response, NextFunction } from "express";
 import { HttpError } from "./error";
 
 async function formParse(req: Request, res: Response, next: NextFunction) {
-  const uploadDir = "uploads";
-  const form = formidable({
-    uploadDir,
-    keepExtensions: true,
-    maxFiles: 1,
-    maxFileSize: 1024 ** 2 * 200, // the default limit is 200KB
-    filter: (part) => part.mimetype?.startsWith("image/") || false,
-  });
-
-  let { fields, files } = await new Promise<{
-    fields: formidable.Fields;
-    files: formidable.Files;
-  }>((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      else resolve({ fields, files });
+  try {
+    const uploadDir = "uploads";
+    const form = formidable({
+      uploadDir,
+      keepExtensions: true,
+      maxFiles: 1,
+      maxFileSize: 1024 ** 2 * 200, // the default limit is 200KB
+      filter: (part) => part.mimetype?.startsWith("image/") || false,
     });
-  });
 
-  let imageMaybeArray = files.image;
-  let image = Array.isArray(imageMaybeArray)
-    ? imageMaybeArray[0]
-    : imageMaybeArray;
-  let filename = image?.newFilename;
+    let { fields, files } = await new Promise<{
+      fields: formidable.Fields;
+      files: formidable.Files;
+    }>((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err);
+        else resolve({ fields, files });
+      });
+    });
 
-  if (!filename) throw new HttpError(400, "missing tongue image");
+    let imageMaybeArray = files.image;
+    let image = Array.isArray(imageMaybeArray)
+      ? imageMaybeArray[0]
+      : imageMaybeArray;
+    let filename = image?.newFilename;
 
-  req.body.filename = filename;
-  next();
+    if (!filename) throw new HttpError(400, "missing tongue image");
+
+    req.body.filename = filename;
+    next();
+  } catch (error) {
+    next(error);
+  }
 }
 
 export class TongueController extends HttpController {
